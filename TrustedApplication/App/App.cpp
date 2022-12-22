894c4022dd5c435ed83ae486ff199b886a00ede2
ValerioGoretti <Valerio.goretti98@gmail.com>
2022-12-22 02:44:25 +0100

    Struttura Enclave
    Manca da importare il protected file system

--- TrustedApplication/App/App.cpp
+++ TrustedApplication/App/App.cpp
@@ -5,6 +5,8 @@
 #include "sgx_urts.h"
 #include "sgx_utils/sgx_utils.h"
 #include "Server.cpp"
+#include "time.h"
+
 
 
 /* Global EID shared by multiple threads */
@@ -15,6 +17,18 @@ void ocall_print(const char* str) {
     printf("%s\n", str);
 }
 
+// OCall implementations
+void get_geo_location(char* str, size_t length) {
+    strncpy(str, "Italia", length);
+}
+
+// OCall implementations
+void get_time(char* t, size_t length) {
+    time_t current_time = time(NULL);
+    char * time_str = ctime(&current_time);
+    time_str[strlen(time_str)-1] = '\0';
+    strncpy(t, time_str, length);
+}
 
 
 int main(int argc, char const *argv[]) {
@@ -22,21 +36,26 @@ int main(int argc, char const *argv[]) {
         std::cout << "Fail to initialize enclave." << std::endl;
         return 1;
     }
-    int ptr;
-    sgx_status_t status = generate_random_number(global_eid, &ptr);
+
+
+    SGX_FILE * file;
+    const char* a = "abc";
+    const char* b = "abc";
+    int* prova;
+    sgx_status_t status = access_protected_resource(global_eid, &file, a, b, prova);
+
+
+    
+
     std::cout << status << std::endl;
     if (status != SGX_SUCCESS) {
         std::cout << "noob" << std::endl;
     }
     
-    printf("Random number: %d\n", ptr);
     std::cout << "Disconnessione" << std::endl;
     DIR *d;
     struct dirent *dir;
-    mainListener();
+    //mainListener();
     return 0;     
-
-
-
 
 }
